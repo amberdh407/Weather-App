d27ccd3d69cb73001daa8d3af0e9e68964f71122
amberdh407 <106625870+amberdh407@users.noreply.github.com>
2022-10-09 17:17:33 -0400

    set attribute for main weather icon to change according to weather searched

--- src/index.js
+++ src/index.js
@@ -83,3 +83,10 @@ function showWeatherDescription(response) {
     response.value.weather.description;
   displayWeatherCondition(showWeatherDescription);
 }
+let iconElement = document
+  .querySelector("#large-icon")
+  .setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+iconElement.setAttribute("alt", response.data.weather[0].description);
