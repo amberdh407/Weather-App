c01e0d4b9548071969f05235d7076d3296fffdf9
amberdh407 <106625870+amberdh407@users.noreply.github.com>
2022-10-09 20:07:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -102,6 +102,7 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#current-temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+let celsiusTemperature = null;
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
